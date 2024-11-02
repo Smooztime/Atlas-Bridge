691f7ef1d1ed815fe9d69a32be238f26eae1ef38
huoxinghua <68455980+huoxinghua@users.noreply.github.com>
2024-11-01 22:09:48 -0600

    the obstacle rolling barrel spawn  and move work

--- Atlas-Bridge/Assets/Scripts/Obstacle/RollingItem.cs
+++ Atlas-Bridge/Assets/Scripts/Obstacle/RollingItem.cs
@@ -4,26 +4,11 @@ using UnityEngine;
 public class RollingItem :MonoBehaviour, IInteractable
 {
     //rolling barrel
-    public float rollSpeed = 5f;
     public void Interact()
     {
-        //if hit the player will die the player
         Debug.Log("get rolling interact");
+        //if hit the player will die the player
         GameManager.Instance.GameOver();
         //make player to to spawn location
-
     }
-   
-
-    void FixedUpdate()
-    {
-
-        transform.position += Vector3.left * rollSpeed * Time.deltaTime;
-
-        if (transform.position.x > 10f)
-        {
-            Destroy(gameObject);
-        }
-    }
-
 }
